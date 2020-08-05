71976de315c3987b91003c8b5b97a9ea5c2cf5bf
Jess <jessbonanno@gmail.com>
2020-08-05 17:31:22 -0400

    theme skeleton set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+// local imports
+import theme from './components/ui/Theme';
+
 import joshSurprised from './assets/images/joshsurprised.png';
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Lambda Network App!</h1>
-      <img src={joshSurprised} width="400" alt="" />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <h1>Welcome to the Lambda Network App!</h1>
+        <img src={joshSurprised} width='400' alt='' />
+      </div>
+    </ThemeProvider>
   );
 }
 
