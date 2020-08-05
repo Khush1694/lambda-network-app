91937797558a8ee6f1f335eb4d6a4d203aef2a7a
Ben Venker <bvemails@gmai.com>
2020-08-05 10:52:46 -0500

    add josh surprised to app...vital

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import joshSurprised from './assets/images/joshsurprised.png';
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Lambda Network App!</h1>
+      <img src={joshSurprised} width="400" alt="" />
     </div>
   );
 }
