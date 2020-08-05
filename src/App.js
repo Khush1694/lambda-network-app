014ef847d369cec25ba2e57668d8c1239e8ab687
Ben Venker <bvemails@gmai.com>
2020-08-05 14:45:44 -0500

    set up useParams

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import PostsPage from './components/PostsPage';
 import SubmitPostPage from './components/SubmitPostPage';
-import Post from './components/Post';
+import PostPage from './components/PostPage';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route exact path="/" component={Home}></Route>
         {/* Will eventually need this to be /profile/:id */}
         <Route path="/profile" component={Profile}></Route>
-        <Route path="/post/:id" component={Post}></Route>
+        <Route path="/post/:id" component={PostPage}></Route>
         <Route path="/posts" component={PostsPage}></Route>
         <Route path="/submit" component={SubmitPostPage}></Route>
       </Switch>
