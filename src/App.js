b874f955b6382ad3c2a9a9020a35014f09262175
Ben Venker <bvemails@gmai.com>
2020-08-05 11:48:46 -0500

    set up some routes and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import joshSurprised from './assets/images/joshsurprised.png';
+import { Route, Switch } from 'react-router';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import PostsPage from './components/PostsPage';
+import SubmitPostPage from './components/SubmitPostPage';
+import Post from './components/Post';
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Lambda Network App!</h1>
-      <img src={joshSurprised} width="400" alt="" />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        {/* Will eventually need this to be /profile/:id */}
+        <Route path="/profile" component={Profile}></Route>
+        <Route path="/post/:id" component={Post}></Route>
+        <Route path="/posts" component={PostsPage}></Route>
+        <Route path="/submit" component={SubmitPostPage}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <>
+      <h1>User Profile</h1>
+      <p>Tell us about yourself...</p>
+    </>
+  );
+};
+
+export default Profile;
