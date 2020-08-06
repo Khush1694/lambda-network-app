13b214963182abfe2a3ad22bb026a285011eddec
Jess <jessbonanno@gmail.com>
2020-08-06 16:28:33 -0400

    header up

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import PostsPage from './components/PostsPage';
 import SubmitPostPage from './components/SubmitPostPage';
 import PostPage from './components/PostPage';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 // local imports
 import theme from './components/ui/Theme';
+import Header from './components/ui/Header';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
+      <div className='App'>
+        <Header />
         <Switch>
-          <Route exact path="/" component={Home}></Route>
+          <Route exact path='/' component={Home}></Route>
           {/* Will eventually need this to be /profile/:id */}
           {/* <Route path="/profile" component={Profile}></Route> */}
-          <Route exacxt path="/profile/edit" component={EditProfile}></Route>
-          <Route path="/post/:id" component={PostPage}></Route>
-          <Route path="/posts" component={PostsPage}></Route>
-          <Route path="/submit" component={SubmitPostPage}></Route>
+          <Route exact path='/profile/edit' component={EditProfile}></Route>
+          <Route path='/post/:id' component={PostPage}></Route>
+          <Route path='/posts' component={PostsPage}></Route>
+          <Route path='/submit' component={SubmitPostPage}></Route>
         </Switch>
       </div>
     </ThemeProvider>
